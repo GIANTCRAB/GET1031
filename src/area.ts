0f3620f04d6bb97bfff0eb43164ee3e410249952
Huiren Woo <giantcrabby@gmail.com>
2019-10-19 21:01:05 +0800

    Change to use Set

--- src/area.ts
+++ src/area.ts
@@ -1,11 +1,16 @@
 import {Point} from "./point";
 
 export class Area {
-    public points: Point[];
+    public points: Set<Point>;
 
     public getAreaCases(): number {
         let totalCases = 0;
         this.points.forEach((point: Point) => totalCases + point.numberOfCase);
         return totalCases;
+    }
+
+    public addPoint(point: Point): Area {
+        this.points.add(point);
+        return this;
     }
 }
