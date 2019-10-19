fef70548586ac192d9c5c4b435440cb3cf1647cb
Huiren Woo <giantcrabby@gmail.com>
2019-10-19 22:16:20 +0800

    Add area list system

---
+++ src/area-list.ts
@@ -0,0 +1,43 @@
+import {Area} from "./area";
+import {Point} from "./point";
+
+export class AreaList {
+    public areas: Set<Area> = new Set<Area>();
+
+    public addArea(area: Area): AreaList {
+        this.areas = this.areas.add(area);
+        return this;
+    }
+
+    public addPoint(point: Point): AreaList {
+        const area: Area = this.checkAreaPointExists(point);
+        if (area !== null) {
+            this.addArea(area.addPoint(point));
+        } else {
+            const newArea: Area = new Area();
+            this.addArea(newArea.addPoint(point));
+        }
+
+        return this;
+    }
+
+    public checkAreaPointExists(point: Point): Area {
+        let foundArea: Area = null;
+
+        this.areas.forEach((area: Area) => {
+            if (area.checkPointInRange(point)) {
+                foundArea = area;
+                return;
+            }
+        });
+
+        return foundArea;
+    }
+
+    public toArray(): Point[][] {
+        const combinedArray: Point[][] = [];
+        this.areas.forEach((area: Area) => combinedArray.push(area.toArray()));
+
+        return combinedArray;
+    }
+}
