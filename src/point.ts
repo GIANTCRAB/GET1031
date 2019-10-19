9aed5575f1b66283e39b1a7e36a1663dbe0c7b95
Huiren Woo <giantcrabby@gmail.com>
2019-10-19 20:50:37 +0800

    Add number of case and hours to inspect attribute

--- src/point.ts
+++ src/point.ts
@@ -2,6 +2,8 @@ export class Point {
     public name: string;
     public x: number;
     public y: number;
+    public numberOfCase: number;
+    public hoursRequiredToInspect: number;
 
     public distanceTo(secondPoint: Point): number {
         return Math.hypot(this.x - secondPoint.x, this.y - secondPoint.y);
