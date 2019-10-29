da12e9be892642845f3bbf79279b395130682016
Huiren Woo <giantcrabby@gmail.com>
2019-10-29 16:54:35 +0800

    Fix total case addition

--- src/area.ts
+++ src/area.ts
@@ -1,11 +1,12 @@
 import {Point} from "./point";
 
 export class Area {
+    public name: string;
     public points: Set<Point> = new Set<Point>();
 
     public getAreaCases(): number {
         let totalCases = 0;
-        this.points.forEach((point: Point) => totalCases + point.numberOfCase);
+        this.points.forEach((point: Point) => totalCases += point.numberOfCase);
         return totalCases;
     }
 
