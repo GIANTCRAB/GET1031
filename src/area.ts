33bf0776f1ed74e16e8e5fb74091f5297408c88a
Huiren Woo <giantcrabby@gmail.com>
2019-11-02 06:08:30 +0800

    Add center calculation

--- src/area.ts
+++ src/area.ts
@@ -3,6 +3,7 @@ import {Point} from "./point";
 export class Area {
     public name: string;
     public points: Set<Point> = new Set<Point>();
+    private center: Point = null;
 
     public getAreaCases(): number {
         let totalCases = 0;
@@ -12,6 +13,7 @@ export class Area {
 
     public addPoint(point: Point): Area {
         this.points = this.points.add(point);
+        this.center = this.calculateCenter();
         return this;
     }
 
@@ -29,7 +31,30 @@ export class Area {
         return inRange;
     }
 
+    public getCenter(): Point {
+        return this.center;
+    }
+
     public toArray(): Point[] {
         return Array.from(this.points);
+    }
+
+    private calculateCenter(): Point {
+        let totalX = 0;
+        let totalY = 0;
+
+        this.points.forEach((point: Point) => {
+            totalX += point.x;
+            totalY += point.y;
+        });
+
+        const avgX = totalX / this.points.size;
+        const avgY = totalY / this.points.size;
+
+        const centerPoint: Point = new Point();
+        centerPoint.x = avgX;
+        centerPoint.y = avgY;
+
+        return centerPoint;
     }
 }
