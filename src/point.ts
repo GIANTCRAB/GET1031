c989d0079a5ee433682925f97c558a7c4652bd83
Huiren Woo <giantcrabby@gmail.com>
2019-11-09 13:17:07 +0800

    Change point to implement interface

--- src/point.ts
+++ src/point.ts
@@ -1,11 +1,24 @@
-export class Point {
-    public name: string;
-    public x: number;
-    public y: number;
-    public numberOfCase: number;
-    public hoursRequiredToInspect: number;
+import {PointInterface} from "./point-interface";
 
-    public distanceTo(secondPoint: Point): number {
-        return Math.hypot(this.x - secondPoint.x, this.y - secondPoint.y);
+export class Point implements PointInterface {
+    private readonly x: number;
+    private readonly y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public distanceTo(secondPoint: PointInterface): number {
+        const DISTANCE_MULTIPLIER = 150;
+        return Math.hypot(this.getX() - secondPoint.getX(), this.getY() - secondPoint.getY()) * DISTANCE_MULTIPLIER;
     }
 }
