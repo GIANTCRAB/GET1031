be3472a5da9753aae4467e86160068f446d1f1ea
Huiren Woo <giantcrabby@gmail.com>
2019-11-11 13:56:11 +0800

    Change to use priority queue

--- src/location-list.ts
+++ src/location-list.ts
@@ -1,4 +1,5 @@
 import {LocationInterface} from "./location-interface";
+import PriorityQueue from "ts-priority-queue/src/PriorityQueue";
 
 export class LocationList {
     private readonly locations: Set<LocationInterface> = new Set<LocationInterface>();
@@ -23,5 +24,40 @@ export class LocationList {
                 // a must be equal to b
                 return 0;
             });
+    }
+
+    public getPriorityQueue(): PriorityQueue<LocationInterface> {
+        const priorityList = this.getByPriority();
+        const maxInspectionFrequency = priorityList[0].getInspectionFrequency();
+        const priorityQueue = new PriorityQueue({
+            comparator: (a: LocationInterface, b: LocationInterface) => {
+                if (a.getInspectionCount() < b.getInspectionCount()) {
+                    return -1;
+                }
+                if (a.getInspectionCount() > b.getInspectionCount()) {
+                    return 1;
+                }
+
+                // break even by number of cases
+                if (a.getNumberOfCases() > b.getNumberOfCases()) {
+                    return -1;
+                }
+                if (a.getNumberOfCases() < b.getNumberOfCases()) {
+                    return 1;
+                }
+                // a must be equal to b
+                return 0;
+            }
+        });
+
+        for (let i = 0; i < maxInspectionFrequency; i++) {
+            priorityList.forEach((location: LocationInterface) => {
+                if (i < location.getInspectionFrequency()) {
+                    priorityQueue.queue(location);
+                }
+            });
+        }
+
+        return priorityQueue;
     }
 }
